6438509291e53a0c99a05ccd2cb07fdec7b70dc1
Guanlun Zhao <guanlunzhao@gmail.com>
2016-09-23 10:15:25 -0500

    Fixed metal welding scripts

--- simulations/metal-welding/scripts/Constants.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    SMOKE: {
-        PARTICLE_NUM: 2000,
-    },
-    SPARK: {
-        PARTICLE_NUM: 2000,
-    },
-}
--- simulations/metal-welding/scripts/StaticPlane.js
+++
@@ -1,6 +0,0 @@
-module.exports = class StaticPlane {
-    constructor(point, normal) {
-        this.point = point;
-        this.normal = normal;
-    }
-}
